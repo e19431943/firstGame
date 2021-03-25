634c982f86467c18e6bf692976ddb57b98211db7
Nofly <e19431943@gmail.com>
2021-03-25 17:41:56 +0800

    完成隨機選取

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@
  var item2 = document.getElementsByClassName('item');
  console.log(items);
  console.log(item2);
- var obj={};
+ var obj=[];
+ var rundomTable=[0,1,2,3,4,5,6,7,8];
+ var targetNum=-1;
  items.forEach(function(item,index)
  {
      obj[index]={
@@ -15,7 +17,10 @@
         if(obj[index].state ==false)
         {
             obj[index].data ="O";
+            obj[index].state =true;
             item.innerHTML = obj[index].data;
+            targetNum =index; //紀錄index 全域變數
+            rundomTable = rundomTable.filter(arrayCheck);
             npc();
         }
         else{
@@ -23,8 +28,32 @@
         }  
     });
  });
+console.log(obj);
+function arrayCheck(item,index)
+{
+    console.log(item);
+    if(item !=  targetNum)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
 
  function npc()
  {
-     console.log(obj.state);
+    rundomTable = rundomTable.sort(function()
+    {
+        return (0.5-Math.random());
+    });
+    obj[rundomTable[0]].data="X";
+    obj[rundomTable[0]].state=true;
+    items[rundomTable[0]].innerHTML = obj[rundomTable[0]].data;
+    targetNum = rundomTable[0];
+    rundomTable = rundomTable.filter(arrayCheck);
+    console.log(rundomTable);
  }
