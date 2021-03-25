a0252cd83447c4a23a211513f5d1cee445917048
Nofly <e19431943@gmail.com>
2021-03-25 20:50:32 +0800

    主要判斷邏輯3_25

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
  var items = document.querySelectorAll('.item');
  var item2 = document.getElementsByClassName('item');
- console.log(items);
- console.log(item2);
+ const row =[[0,1,2],[3,4,5],[6,7,8]];
+ const col =[[0,3,6],[1,4,7],[2,5,8]];
+ const tilted =[[0,4,8],[2,4,6]];
+ var npcFlag = false;;
  var obj=[];
  var rundomTable=[0,1,2,3,4,5,6,7,8];
  var targetNum=-1;
@@ -21,6 +23,7 @@
             item.innerHTML = obj[index].data;
             targetNum =index; //紀錄index 全域變數
             rundomTable = rundomTable.filter(arrayCheck);
+            npcFlag =true;
             npc();
         }
         else{
@@ -28,10 +31,10 @@
         }  
     });
  });
-console.log(obj);
+
+
 function arrayCheck(item,index)
 {
-    console.log(item);
     if(item !=  targetNum)
     {
         return true;
@@ -46,14 +49,213 @@ function arrayCheck(item,index)
 
  function npc()
  {
-    rundomTable = rundomTable.sort(function()
+    /*row判斷 */
+    rowCheck();
+    
+    
+    //*隨機區 */
+    if(false)
     {
-        return (0.5-Math.random());
-    });
-    obj[rundomTable[0]].data="X";
-    obj[rundomTable[0]].state=true;
-    items[rundomTable[0]].innerHTML = obj[rundomTable[0]].data;
-    targetNum = rundomTable[0];
-    rundomTable = rundomTable.filter(arrayCheck);
-    console.log(rundomTable);
+
+        rundomTable = rundomTable.sort(function()
+        {
+            return (0.5-Math.random());
+        });
+        obj[rundomTable[0]].data="X";
+        obj[rundomTable[0]].state=true;
+        items[rundomTable[0]].innerHTML = obj[rundomTable[0]].data;
+        targetNum = rundomTable[0];
+        rundomTable = rundomTable.filter(arrayCheck);
+        console.log(row.length);
+    }
  }
+
+ function rowCheck()/*row判斷 */
+ {
+     if(npcFlag == false){ return 0}
+     for(let i=0 ;i<row.length;i++)
+     {
+        let rowNum =0;
+        for(let j = 0 ;j<row[i].length ; j++)
+        {
+            if(obj[row[i][j]].data == "O"){
+                rowNum++;
+            }
+            else if(obj[row[i][j]].data == "X")
+            {
+                rowNum--;
+            }
+            else
+            {
+                rowNum=rowNum+0;
+            }
+        }
+        if(rowNum == -2)
+        {  
+            row[i].forEach(function(num)
+            {
+                console.log(obj[num].state);
+               if(obj[num].data == "")
+               {
+                   obj[num].data ="X";
+                   obj[num].state = true;
+                   targetNum =obj[num].index;
+                   items[obj[num].index].innerHTML = "X";
+                   rundomTable = rundomTable.filter(arrayCheck);
+                   npcFlag =false;
+               }
+            });
+            return ;
+        }else if(rowNum == 2)
+        {
+            row[i].forEach(function(num)
+            {
+                console.log(obj[num].state);
+               if(obj[num].data == "")
+               {
+                   obj[num].data ="X";
+                   obj[num].state = true;
+                   targetNum =obj[num].index;
+                   items[obj[num].index].innerHTML = "X";
+                   rundomTable = rundomTable.filter(arrayCheck);
+                   npcFlag =false;
+               }
+            });
+            console.log("repkdd");
+            return ;
+        }
+    }
+    colCheck();
+ }
+ function colCheck()/*col判斷 */
+ {
+    if(npcFlag == false){ return 0}
+     for(let i=0 ;i<col.length;i++)
+     {
+        let colNum =0;
+        for(let j = 0 ;j<col[i].length ; j++)
+        {
+            if(obj[col[i][j]].data == "O"){
+                colNum++;
+            }
+            else if(obj[col[i][j]].data == "X")
+            {
+                colNum--;
+            }
+            else
+            {
+                colNum=colNum+0;
+            }
+        }
+        if(colNum == -2)
+        {  
+            
+            col[i].forEach(function(num)
+            {
+                console.log(obj[num].state);
+               if(obj[num].data == "")
+               {
+                   obj[num].data ="X";
+                   obj[num].state = true;
+                   targetNum =obj[num].index;
+                   items[obj[num].index].innerHTML = "X";
+                   rundomTable = rundomTable.filter(arrayCheck);
+                   npcFlag =false;
+               }
+            });
+            return;
+        }
+        else if(colNum == 2)
+        {  
+            
+            col[i].forEach(function(num)
+            {
+                console.log(obj[num].state);
+               if(obj[num].data == "")
+               {
+                   obj[num].data ="X";
+                   obj[num].state = true;
+                   targetNum =obj[num].index;
+                   items[obj[num].index].innerHTML = "X";
+                   rundomTable = rundomTable.filter(arrayCheck);
+                   npcFlag =false;
+               }
+            });
+            return;
+        }
+    }
+    
+    tiltedCheck();
+ }
+ function tiltedCheck()/*tilted 判斷 */
+ {
+    if(npcFlag == false){ return 0}
+     for(let i=0 ;i<tilted.length;i++)
+     {
+        let tiltedNum =0;
+        for(let j = 0 ;j<tilted[i].length ; j++)
+        {
+            if(obj[tilted[i][j]].data == "O"){
+                tiltedNum++;
+            }
+            else if(obj[tilted[i][j]].data == "X")
+            {
+                tiltedNum--;
+            }
+            else
+            {
+                tiltedNum=tiltedNum+0;
+            }
+        }
+        if(tiltedNum == -2)
+        {  
+            
+            tilted[i].forEach(function(num)
+            {
+                console.log(obj[num].state);
+               if(obj[num].data == "")
+               {
+                   obj[num].data ="X";
+                   obj[num].state = true;
+                   targetNum =obj[num].index;
+                   items[obj[num].index].innerHTML = "X";
+                   rundomTable = rundomTable.filter(arrayCheck);
+                   npcFlag =false;
+               }
+            });
+            return;
+        }
+        if(tiltedNum == 2)
+        {  
+            
+            tilted[i].forEach(function(num)
+            {
+                console.log(obj[num].state);
+               if(obj[num].data == "")
+               {
+                   obj[num].data ="X";
+                   obj[num].state = true;
+                   targetNum =obj[num].index;
+                   items[obj[num].index].innerHTML = "X";
+                   rundomTable = rundomTable.filter(arrayCheck);
+                   npcFlag =false;
+               }
+            });
+            return;
+        }
+    }
+    runCheck();
+ }
+ function runCheck()//*隨機區 */
+{
+    rundomTable = rundomTable.sort(function()
+        {
+            return (0.5-Math.random());
+        });
+        obj[rundomTable[0]].data="X";
+        obj[rundomTable[0]].state=true;
+        items[rundomTable[0]].innerHTML = obj[rundomTable[0]].data;
+        targetNum = rundomTable[0];
+        rundomTable = rundomTable.filter(arrayCheck);
+        console.log(row.length);
+}
